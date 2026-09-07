d293c42fa506b525f8d6bb77d18535435b561544
cat-energies-cattenom <cat-energies-cattenom@edf.fr>
2026-09-07 08:50:53 +0200

    Ajout de la configuration centrale CAT Energies Cattenom

---
+++ assets/cat-energies/js/cat-config.js
@@ -0,0 +1,22 @@
+window.CAT_CONFIG = Object.freeze({
+  brand: "CAT Énergies",
+  localName: "CAT Énergies Cattenom",
+  contactEmail: "cat-energies-cattenom@edf.fr",
+  portalUrl: "https://cat-energies.github.io/edf/dpn/cattenom/",
+  adhesionUrl: "https://cat-energies.github.io/edf/dpn/cattenom/adhesion/Bulletin%20d'adh%C3%A9sion_Cattenom.html",
+  logoUrl: "https://cat-energies.github.io/edf/logo-cat-energies.svg",
+  goatCounter: {
+    slug: "cat-energies-cattenom",
+    endpoint: "https://cat-energies-cattenom.goatcounter.com/count",
+    script: "https://gc.zgo.at/count.js"
+  },
+  communicationTypes: {
+    communique: { label: "Communiqué", theme: "information" },
+    information: { label: "Information", theme: "information" },
+    mobilisation: { label: "Mobilisation", theme: "mobilisation" },
+    adhesion: { label: "Adhésion", theme: "adhesion" },
+    sondage: { label: "Sondage / participation", theme: "sondage" },
+    guide: { label: "Guide pratique", theme: "guide" },
+    "edf-sa": { label: "EDF SA / IEG", theme: "edf-sa" }
+  }
+});
